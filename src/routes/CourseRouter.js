617b2a1c9124ffe061afdb84afe0f1b6e47fa3fc
wenses-pl <wpenadillol@agiletech.com.pe>
2022-06-29 13:39:40 -0500

    update

--- src/routes/CourseRouter.js
+++ src/routes/CourseRouter.js
@@ -1,18 +1,20 @@
 const { Router } = require('express')
 
 const {
-    createCourse,
-    deleteCourse,
-    getAllCourses,
-    getCourse
-  } = require('../controllers/CourseControllers')
+  createCourse,
+  deleteCourse,
+  getAllCourses,
+  getCourse
+} = require('../controllers/CourseControllers')
 
 const { fieldValidator } = require('../middlewares/fieldValidator')
 const { validateJWT } = require('../middlewares/validateJWT')
 
 const router = Router()
 
-router.route('/').get([validateJWT],getAllCourses).post([validateJWT],createCourse)
-router.route('/:courseId').get([validateJWT],getCourse).delete([validateJWT],deleteCourse)
+router.use(validateJWT)
+
+router.route('/').get(getAllCourses).post(createCourse)
+router.route('/:courseId').get(getCourse).delete(deleteCourse)
 
 module.exports = router
